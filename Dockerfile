6ba5bb2e46f13b290aa81c8ceffc97d871687088
xwx <794760487@qq.com>
2021-08-19 17:51:13 +0800

    add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM java:8
+COPY *.jar /app.jar
+CMD ["--server.port=8081"]
+EXPOSE 8081
+ENTRYPOINT ["java","-jar","/app.jar"]
+
--- src/test/java/com/fred/blog/codenotes/CodenotesApplicationTests.java
+++ src/test/java/com/fred/blog/codenotes/CodenotesApplicationTests.java
@@ -1,12 +1,7 @@
 package com.fred.blog.codenotes;
 
-import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.fred.blog.codenotes.utils.MD5Utils;
-
-import java.util.HashMap;
 
 @SpringBootTest
 class CodenotesApplicationTests {
